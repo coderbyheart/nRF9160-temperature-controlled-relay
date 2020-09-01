14cdf95484637e34f4e4ee45b573fd8fb35fa83a
Markus Tacker <Markus.Tacker@NordicSemi.no>
2020-09-01 13:32:12 +0200

    feat: add cellular connectivity and init AWS IoT

--- src/main.c
+++ src/main.c
@@ -14,8 +14,16 @@
 #include <dk_buttons_and_leds.h>
 #include <cJSON.h>
 #include <cJSON_os.h>
+#include <net/aws_iot.h>
+#include <power/reboot.h>
+#include <date_time.h>
+#include <dfu/mcuboot.h>
+#include <modem/lte_lc.h>
+#include <modem/bsdlib.h>
+#include <bsd.h>
 
 static struct k_delayed_work read_sensor_data_work;
+K_SEM_DEFINE(lte_connected, 0, 1);
 
 uint8_t interval = 10;
 double threshold = 24;
@@ -87,6 +95,185 @@ static void read_sensor_data_work_fn(struct k_work *work)
 	k_delayed_work_submit(&read_sensor_data_work, K_SECONDS(interval));
 }
 
+void aws_iot_event_handler(const struct aws_iot_evt *const evt)
+{
+	switch (evt->type) {
+	case AWS_IOT_EVT_CONNECTING:
+		printk("AWS_IOT_EVT_CONNECTING\n");
+		break;
+	case AWS_IOT_EVT_CONNECTED:
+		printk("AWS_IOT_EVT_CONNECTED\n");
+
+		if (evt->data.persistent_session) {
+			printk("Persistent session enabled\n");
+		}
+
+		/** Successfully connected to AWS IoT broker, mark image as
+		 *  working to avoid reverting to the former image upon reboot.
+		 */
+		boot_write_img_confirmed();
+
+		/** Send version number to AWS IoT broker to verify that the
+		 *  FOTA update worked.
+		 */
+		// k_delayed_work_submit(&shadow_update_version_work, K_NO_WAIT);
+
+		/** Start sequential shadow data updates.
+		 */
+		// k_delayed_work_submit(&shadow_update_work, K_SECONDS(CONFIG_PUBLICATION_INTERVAL_SECONDS));
+
+		int err = lte_lc_psm_req(true);
+		if (err) {
+			printk("Requesting PSM failed, error: %d\n", err);
+		}
+
+		break;
+	case AWS_IOT_EVT_READY:
+		printk("AWS_IOT_EVT_READY\n");
+		break;
+	case AWS_IOT_EVT_DISCONNECTED:
+		printk("AWS_IOT_EVT_DISCONNECTED\n");
+		// k_delayed_work_cancel(&shadow_update_work);
+		break;
+	case AWS_IOT_EVT_DATA_RECEIVED:
+		printk("AWS_IOT_EVT_DATA_RECEIVED\n");
+		break;
+	case AWS_IOT_EVT_FOTA_START:
+		printk("AWS_IOT_EVT_FOTA_START\n");
+		break;
+	case AWS_IOT_EVT_FOTA_ERASE_PENDING:
+		printk("AWS_IOT_EVT_FOTA_ERASE_PENDING\n");
+		printk("Disconnect LTE link or reboot\n");
+		err = lte_lc_offline();
+		if (err) {
+			printk("Error disconnecting from LTE\n");
+		}
+		break;
+	case AWS_IOT_EVT_FOTA_ERASE_DONE:
+		printk("AWS_FOTA_EVT_ERASE_DONE\n");
+		printk("Reconnecting the LTE link");
+		err = lte_lc_connect();
+		if (err) {
+			printk("Error connecting to LTE\n");
+		}
+		break;
+	case AWS_IOT_EVT_FOTA_DONE:
+		printk("AWS_IOT_EVT_FOTA_DONE\n");
+		printk("FOTA done, rebooting device\n");
+		aws_iot_disconnect();
+		sys_reboot(0);
+		break;
+	case AWS_IOT_EVT_FOTA_DL_PROGRESS:
+		printk("AWS_IOT_EVT_FOTA_DL_PROGRESS, (%d%%)", evt->data.fota_progress);
+	case AWS_IOT_EVT_ERROR:
+		printk("AWS_IOT_EVT_ERROR, %d\n", evt->data.err);
+		break;
+	default:
+		printk("Unknown AWS IoT event type: %d\n", evt->type);
+		break;
+	}
+}
+
+static void lte_handler(const struct lte_lc_evt *const evt)
+{
+	switch (evt->type) {
+	case LTE_LC_EVT_NW_REG_STATUS:
+		if ((evt->nw_reg_status != LTE_LC_NW_REG_REGISTERED_HOME) &&
+		     (evt->nw_reg_status != LTE_LC_NW_REG_REGISTERED_ROAMING)) {
+			break;
+		}
+
+		printk("Network registration status: %s\n",
+			evt->nw_reg_status == LTE_LC_NW_REG_REGISTERED_HOME ?
+			"Connected - home network" : "Connected - roaming");
+
+		k_sem_give(&lte_connected);
+		break;
+	case LTE_LC_EVT_PSM_UPDATE:
+		printk("PSM parameter update: TAU: %d, Active time: %d\n",
+			evt->psm_cfg.tau, evt->psm_cfg.active_time);
+		break;
+	case LTE_LC_EVT_EDRX_UPDATE: {
+		char log_buf[60];
+		ssize_t len;
+
+		len = snprintf(log_buf, sizeof(log_buf),
+			       "eDRX parameter update: eDRX: %f, PTW: %f",
+			       evt->edrx_cfg.edrx, evt->edrx_cfg.ptw);
+		if (len > 0) {
+			printk("%s\n", log_buf);
+		}
+		break;
+	}
+	case LTE_LC_EVT_RRC_UPDATE:
+		printk("RRC mode: %s\n",
+			evt->rrc_mode == LTE_LC_RRC_MODE_CONNECTED ?
+			"Connected" : "Idle");
+		break;
+	case LTE_LC_EVT_CELL_UPDATE:
+		printk("LTE cell changed: Cell ID: %d, Tracking area: %d\n",
+			evt->cell.id, evt->cell.tac);
+		break;
+	default:
+		break;
+	}
+}
+
+static void modem_configure(void)
+{
+	int err;
+
+	if (IS_ENABLED(CONFIG_LTE_AUTO_INIT_AND_CONNECT)) {
+		/* Do nothing, modem is already configured and LTE connected. */
+	} else {
+		err = lte_lc_init_and_connect_async(lte_handler);
+		if (err) {
+			printk("Modem could not be configured, error: %d\n",
+				err);
+			return;
+		}
+	}
+}
+
+static void at_configure(void)
+{
+	int err;
+
+	err = at_notif_init();
+	__ASSERT(err == 0, "AT Notify could not be initialized.");
+	err = at_cmd_init();
+	__ASSERT(err == 0, "AT CMD could not be established.");
+}
+
+static void bsd_lib_modem_dfu_handler(void)
+{
+	int err;
+
+	err = bsdlib_init();
+
+	switch (err) {
+	case MODEM_DFU_RESULT_OK:
+		printk("Modem update suceeded, reboot\n");
+		sys_reboot(SYS_REBOOT_COLD);
+		break;
+	case MODEM_DFU_RESULT_UUID_ERROR:
+	case MODEM_DFU_RESULT_AUTH_ERROR:
+		printk("Modem update failed, error: %d\n", err);
+		printk("Modem will use old firmware\n");
+		sys_reboot(SYS_REBOOT_COLD);
+		break;
+	case MODEM_DFU_RESULT_HARDWARE_ERROR:
+	case MODEM_DFU_RESULT_INTERNAL_ERROR:
+		printk("Modem update malfunction, error: %d, reboot\n", err);
+		sys_reboot(SYS_REBOOT_COLD);
+		break;
+	default:
+		break;
+	}
+
+	at_configure();
+}
+
 static void work_init(void)
 {
 	k_delayed_work_init(&read_sensor_data_work, read_sensor_data_work_fn);
@@ -94,17 +281,19 @@ static void work_init(void)
 }
 
 void main(void) {
+	int err;
+
 	// Initialize Buttons
-	int buttonError = dk_buttons_init(button_handler);
-	if (buttonError != 0) {
-		printf("dk_buttons_init, error: %d", buttonError);
+	err = dk_buttons_init(button_handler);
+	if (err != 0) {
+		printf("dk_buttons_init, error: %d", err);
 		return;
 	}
 
 	// Initialize LEDs
-	int ledError = dk_leds_init();
-	if (ledError != 0) {
-		printf("ledError, error: %d", ledError);
+	err = dk_leds_init();
+	if (err != 0) {
+		printf("ledError, error: %d", err);
 		return;
 	}
 
@@ -118,5 +307,23 @@ void main(void) {
 
 	cJSON_Init();
 
+	bsd_lib_modem_dfu_handler();
+
+	err = aws_iot_init(NULL, aws_iot_event_handler);
+	if (err) {
+		printk("AWS IoT library could not be initialized, error: %d\n", err);
+		return;
+	}
+
 	work_init();
+
+	modem_configure();
+
+	err = modem_info_init();
+	if (err) {
+		printk("Failed initializing modem info module, error: %d\n",
+			err);
+	}
+
+	k_sem_take(&lte_connected, K_FOREVER);
 }
